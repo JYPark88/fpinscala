94707f07515cfbf78c3e78419798535164cd89be
JYPark88 <hiphopgn@naver.com>
2016-03-10 02:56:14 +0900

    state 6.3

---
+++ src/state/State.scala
@@ -0,0 +1,56 @@
+package state
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    if (i < 0) (-(i + 1), r) else (i, r)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / Int.MaxValue.toDouble + 1, r)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r2)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r1) = double(rng)
+    val (i, r2) = r1.nextInt
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  def int(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) {
+      (List(), rng)
+    } else {
+      val (i1, r1) = rng.nextInt
+      val (i2, r2) = int(count - 1)(r1)
+      (i1 :: i2, r2)
+    }
+  }
+}
