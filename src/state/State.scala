30cbca5f041bb65946f4459b4573d2e1777e2df0
JYPark88 <hiphopgn@naver.com>
2016-03-11 02:39:20 +0900

    state - flatmap

--- src/state/State.scala
+++ src/state/State.scala
@@ -94,4 +94,27 @@ object RNG {
 
   def _ints(count: Int): Rand[List[Int]] =
     sequence(List.fill(count)(int))
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng2) = f(rng)
+      g(a)(rng2)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+  }
+
+  def _map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def _map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => _map(rb)(b => f(a,b)))
+
+  def rollDie: Rand[Int] = nonNegativeLessThan(6)
+
+  def rollDieFix: Rand[Int] = _map(nonNegativeLessThan(6))(_ + 1)
 }
