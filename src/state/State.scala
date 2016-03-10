ee61024da182e89cdabc2a9810c357b18a83c1d5
JYPark88 <hiphopgn@naver.com>
2016-03-11 02:05:04 +0900

    state - map, map2

--- src/state/State.scala
+++ src/state/State.scala
@@ -27,7 +27,7 @@ object RNG {
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
     val (i, r1) = rng.nextInt
-    val (d, r2) = double(r2)
+    val (d, r2) = double(r1)
     ((i, d), r2)
   }
 
@@ -44,13 +44,54 @@ object RNG {
     ((d1, d2, d3), r3)
   }
 
-  def int(count: Int)(rng: RNG): (List[Int], RNG) = {
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
     if (count == 0) {
       (List(), rng)
     } else {
       val (i1, r1) = rng.nextInt
-      val (i2, r2) = int(count - 1)(r1)
+      val (i2, r2) = ints(count - 1)(r1)
       (i1 :: i2, r2)
     }
   }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def _double: Rand[Double] =
+    map(nonNegativeInt)(_ / Int.MaxValue.toDouble + 1)
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a, b), rng3)
+    }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] =
+    both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] =
+    both(double, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  def _ints(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
 }
